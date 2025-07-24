898f56a84e3b2dfaf0c6bc2dad1cb5a9531bcb68
Saurav Kumar <coolsaurav34@gmail.com>
2025-07-24 19:18:26 +0530

    Add test CORS endpoint to verify headers

---
+++ pages/api/test-cors.js
@@ -0,0 +1,25 @@
+export default function handler(req, res) {
+  // Handle OPTIONS preflight request
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    res.setHeader('Access-Control-Max-Age', '86400')
+    return res.status(200).end()
+  }
+
+  // Add CORS headers to all responses
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.setHeader('Access-Control-Max-Age', '86400')
+
+  return res.status(200).json({ 
+    message: 'CORS test successful',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    }
+  })
+} 
